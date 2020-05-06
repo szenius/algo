c2965b42ec171bc08c0a5df8737a549e20ee8ffe
Sze Ying <ting.szeying@gmail.com>
2020-05-06 23:02:05 +0800

    refactor caesar salad

--- others/caesar_cipher.py
+++ others/caesar_cipher.py
@@ -13,18 +13,21 @@ def caesar_cipher(str, num):
     for char in str:
         ascii_val = ord(char)
         if ascii_val >= ASCII_a and ascii_val <= ASCII_z:
-            new_ascii_val = ascii_val + num
-            if new_ascii_val > ASCII_z:
-                new_ascii_val = new_ascii_val - ASCII_z + ASCII_a - 1
+            new_ascii_val = shift_ascii_val(ascii_val, ASCII_a, ASCII_z, num)
             result += chr(new_ascii_val)
         elif ascii_val >= ASCII_A and ascii_val <= ASCII_Z:
-            new_ascii_val = ascii_val + num
-            if new_ascii_val > ASCII_Z:
-                new_ascii_val = new_ascii_val - ASCII_Z + ASCII_A - 1
+            new_ascii_val = shift_ascii_val(ascii_val, ASCII_A, ASCII_Z, num)
             result += chr(new_ascii_val)
         else:
             result += char
     return result
+
+
+def shift_ascii_val(val, lower, upper, shift):
+    new_val = val + num
+    if new_val > upper:
+        new_val = new_val - upper - 1 + lower
+    return new_val
 
 
 str, num = sys.argv[1], int(sys.argv[2])
